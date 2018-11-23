f4f00f93f553dbaf96dc9b25537442cab04153e6
David Hoppe <dh@davidhoppe.de>
2018-11-23 23:30:24 +0100

    Added first sample implementation

---
+++ examples/all_boards.php
@@ -0,0 +1,17 @@
+<?php
+require "vendor/autoload.php";
+
+use Trello\Client;
+
+require ".config";
+
+$client = new Client();
+$client->authenticate($key, $token, Client::AUTH_URL_CLIENT_ID);
+
+$boards = $client->api('boards')->all();
+//$boards = $client->api('boards')->lists()->all($boardId);
+
+foreach ($boards as $board) {
+    print_r($board);
+}
+
